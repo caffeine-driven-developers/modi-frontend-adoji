59c90f14ebe9d5fb4a65eea2eaf3b6d7b936642b
Dong-jin Ahn <adoji92@gmail.com>
2020-02-06 23:20:13 +0900

    Improve validateAccessToken

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -14,11 +14,25 @@ export function getGoogleLoginResponseFromLocalStorage(): GoogleLoginResponse | 
 export async function validateAccessToken(
   glr: GoogleLoginResponse,
 ): Promise<boolean> {
-  const res = await axios.get(
-    `https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=${glr.accessToken}`,
-  );
-  const isValidToken = isNil(res.data.error_description);
-  return isValidToken;
+  let hasError = false;
+  try {
+    // NOTE: access token이 만료되었으면 400 error라서 catch에서 잡힘
+    await axios.get(
+      `https://www.googleapis.com/oauth2/v3/tokeninfo?access_token=${glr.accessToken}`,
+    );
+  } catch (error) {
+    hasError = true;
+  }
+
+  return !hasError;
+}
+
+export async function isLoggedIn(): Promise<boolean> {
+  const glr = getGoogleLoginResponseFromLocalStorage();
+  if (isNil(glr)) {
+    return false;
+  }
+  return true;
 }
 
 export async function validateUser(
@@ -32,14 +46,7 @@ export async function validateUser(
     };
   }
 
-  let isTokenValid = false;
-  try {
-    isTokenValid = await validateAccessToken(glr);
-    // NOTE: access token이 만료되었으면 400 error라서 catch에서 잡힘
-  } catch (error) {
-    isTokenValid = false;
-  }
-
+  const isTokenValid = await validateAccessToken(glr);
   if (!isTokenValid) {
     return {
       hasAuth: false,
