572c5c476fefb084c070f22acb039c79bfba658b
Dong-jin Ahn <adoji92@gmail.com>
2020-02-07 22:49:36 +0900

    Init firebase with .env

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,23 @@ import configureStore from './stores';
 import { ThemeProvider } from 'styled-components';
 import { themes } from 'react95';
 
+import * as firebase from 'firebase/app';
+import 'firebase/analytics';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+};
+firebase.initializeApp(firebaseConfig);
+firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL);
+
 const store = configureStore();
 
 ReactDOM.render(
